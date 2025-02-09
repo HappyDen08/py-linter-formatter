2614fcc15a96a18230b1cccedd68b749485a9556
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-02-09 15:32:42 +0200

    'Solution_error_fix'

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 def format_linter_error(error: dict) -> dict:
     return {"line": error["line_number"], "column": error["column_number"],
-            "message": error["text"], "name": error["code"], "source": "flake8"}
+            "message": error["text"], "name": error["code"],
+            "source": "flake8"}
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -11,5 +12,5 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 
 def format_linter_report(linter_report: dict) -> list:
-    return [format_single_linter_file(path ,error)
+    return [format_single_linter_file(path, error)
             for path, error in linter_report.items()]
